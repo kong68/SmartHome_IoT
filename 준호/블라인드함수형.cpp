1759b0ff32b4aeb0d7557f463263d5109e28f96b
junn <60811775+toodox@users.noreply.github.com>
2022-06-02 21:07:05 +0900

    Update 블라인드함수형.cpp

--- 준호/블라인드함수형.cpp
+++ 준호/블라인드함수형.cpp
@@ -1,12 +1,12 @@
 #include "mbed.h"
 #include "Servo.h"
 
-Servo blind(D3);                                    // 서보모터 변수 선언 > 블라인드
-AnalogIn cds_sensor(A0);                            // 조도센서 변수 선언
+Servo blind(D12);                                    // 서보모터 변수 선언 > 블라인드
+AnalogIn cds_sensor(A1);                            // 조도센서 변수 선언
 
 Serial pc(SERIAL_TX,SERIAL_RX);                     // 시리얼통신
 
-int flag = 0;                                                           // 블라인드 상태를 저장하는 변수, 현재 닫혀있음
+int blind_flag = 0;                                                           // 블라인드 상태를 저장하는 변수, 현재 닫혀있음
 
 
 void blind_control() {
@@ -22,16 +22,16 @@ void blind_control() {
         
         if (cds_sensor.read() > 0.7f)          // 1. 주변이 어두우면
         {
-                if(flag == 0) {             // 2. 블라인드가 닫혀있으면
+                if(blind_flag == 0) {             // 2. 블라인드가 닫혀있으면
                         blind.write(1.0f);      // 블라인드 오픈
-                        flag = 1;               // 오픈 상태 저장
+                        blind_flag = 1;               // 오픈 상태 저장
                 }
         }
         else 
         {                                   // 1. 주변이 밝으면
-                if(flag == 1) {             // 2. 블라인드가 열려 있으면
+                if(blind_flag == 1) {             // 2. 블라인드가 열려 있으면
                         blind.write(0.5f);      // 블라인드 클로즈
-                        flag = 0;               // 클로즈 상태 저장
+                        blind_flag = 0;               // 클로즈 상태 저장
                 }
         }
 }
